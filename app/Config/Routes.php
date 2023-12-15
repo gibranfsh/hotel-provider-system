b49dcbe46dc5bd8fe75d3561a02646a8de29a1fc
nadinealiya <16521289@std.stei.itb.ac.id>
2023-12-15 17:57:04 +0700

    design dashboard

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/booking', 'Booking::index');
+$routes->get('/explore', 'Explore::index');
+$routes->get('/hotel', 'Hotel::index');
+$routes->get('/payment', 'Payment::index');
+$routes->get('/login', 'Login::index');
