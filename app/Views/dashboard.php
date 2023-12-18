1e0c8e90d65af125f8d11833d348c1ebbe3d8fc5
nadinealiya <16521289@std.stei.itb.ac.id>
2023-12-18 20:30:12 +0700

    benerin button login di dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -16,15 +16,34 @@
             <img class="block relative bg-center items-center mx-auto w-80" src="/img/hoteloka.png" alt="hoteloka" />
         </div>
     </div>
-    <div class="text-center mt-24">
-        <p class="text-white font-bold text-center;">Portal pintar untuk petualangan Anda</p>
+    <div class="text-center mt-32 py-4">
+        <p class="text-white font-bold">Portal pintar untuk petualangan Anda</p>
     </div>
     <div class="text-white mx-auto w-[70%] rounded-lg bg-[#FF834F] p-8 ">
         <p class="text-justify">Selamat datang di Hoteloka ! Dapatkan pengalaman menginap yang tak terlupakan dengan pilihan hotel terbaik di seluruh dunia melalui platform pemesanan online kami! Temukan kenyamanan dan kelas atas dengan harga terbaik, serta nikmati kemudahan memesan secara cepat dan aman. Mulailah petualangan Anda sekarang dan temukan destinasi impian Anda bersama layanan pemesanan hotel online kami</p>
     </div>
-    <div class="mx-auto w-24 h-1 text-center rounded-lg bg-[#FF834F] p-8 mt-10">
-        <button class="text-white"><a href='/login'>Login</a></button>
-    </div>
+    <style>
+        .custom-button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100px;
+            /* Adjust the width as needed */
+            height: 40px;
+            /* Adjust the height as needed */
+            margin: 20px auto 0;
+            /* Centers the button horizontally and adds space at the top */
+        }
+
+        .custom-button a {
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+
+    <button class="rounded-full bg-[#FF834F] custom-button">
+        <a href='/login'>Login</a>
+    </button>
 </body>
 
 </html>
--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -6,17 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        .flex-grow-1 {
+            /* Your main content styles */
+            flex-grow: 1;
+            margin-bottom: 100px;
+            /* Add margin at the bottom to move the footer down */
+        }
+
+        footer {
+            width: 100%;
+            background-color: #343a40;
+            color: #fff;
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        .footer-container {
+            max-width: 500px;
+            /* Adjust the max-width to make the footer less wide */
+            margin: 0 auto;
+        }
+    </style>
 </head>
 
-<body class="d-flex flex-column min-vh-100">
+<body class="d-flex flex-column">
 
     <div class="flex-grow-1">
         <!-- Your main content goes here -->
     </div>
 
-    <footer class="w-100 bg-dark text-light py-4">
-        <div class="container text-center">
-            <p class="mb-0">Copyright Gibeh - Ilma - Nadine</p>
+    <footer>
+        <div class="container-fluid bg-dark text-light py-4">
+            <div class="footer-container">
+                <p class="mb-0">Copyright Gibeh - Ilma - Nadine</p>
+            </div>
         </div>
     </footer>
 
